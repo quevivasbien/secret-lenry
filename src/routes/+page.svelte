a162877cb0d7094e252745a959a48590831ae0c8
Dillon Jensen <dillonj0@stanford.edu>
2025-08-30 11:37:59 -0700

    lenry homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,12 @@
-<script lang="ts">
-    import Button from "$lib/components/Button.svelte";
-    import Deck from "$lib/components/Deck.svelte";
-    import type { CardInfo } from "$lib/interfaces";
-
-    let cardsBack: CardInfo[] = $state([
-        { front: "a-front", back: "a-back" },
-        { front: "b-front", back: "b-back" },
-        { front: "c-front", back: "c-back" },
-    ]);
-    let cardsFront: CardInfo[] = $state([]);
-
-    function drawCard() {
-        const card = cardsBack.pop();
-        if (card) {
-            cardsFront.push(card);
-        }
-    }
-
-    function shuffleArray<T>(array: T[]): T[] {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    function resetCards() {
-        cardsBack = shuffleArray([...cardsBack, ...cardsFront]);
-        cardsFront = [];
-    }
+<script>
+    import lenry from "$lib/assets/secretLenry_pixelated.png";
 </script>
 
-<div class="flex flex-row justify-center content-center gap-4">
-    <Deck cards={cardsBack} side="back" />
-    <Deck cards={cardsFront} side="front" />
-</div>
+<style>
+    a {
+        float: center;
+        position: fixed;
+    }
+</style>
 
-<Button onclick={drawCard}>Draw Card</Button>
-<Button onclick={resetCards}>Reset</Button>
+<a href="/game"><img src={lenry} alt='secret lenry'/></a>
