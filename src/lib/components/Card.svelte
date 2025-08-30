f09361e0291e49946cff3dbcb4e19e160b823516
quevivasbien <mckaydjensen@gmail.com>
2025-08-30 12:53:03 -0400

    worked on card formatting

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
-    import { sharedState } from "$lib/state.svelte";
-
-    let { front, back, side = "back" }: {
-        front: string,
-        back: string,
-        side: "front" | "back",
+    let {
+        front,
+        back,
+        side = "back",
+    }: {
+        front: string;
+        back: string;
+        side: "front" | "back";
     } = $props();
 
-    let { cardWidth, cardHeight } = $derived.by(() => {
-        if (sharedState.uiScale === "small") {
-            return { cardWidth: 60, cardHeight: 80 };
-        }
-        return { cardWidth: 90, cardHeight: 120 };
-    });
+    const MAX_ROTATION_DEG = 3;
+    const rotation = -MAX_ROTATION_DEG + 2 * Math.random() * MAX_ROTATION_DEG;
 </script>
 
-{#if side === "front"}
-    <div class="p-4 border rounded shadow" style="width: {cardWidth}px; height: {cardHeight}px">
-        {front}
-    </div>
-{:else}
-    <div class="p-4 border rounded shadow bg-gray-100" style="width: {cardWidth}px; height: {cardHeight}px">
-        {back}
-    </div>
-{/if}
+<div
+    class="m-1 p-1 border border-gray-200 rounded shadow flex flex-col justify-center content-center select-none w-[90px] h-[120px] sm:w-[135px] sm:h-[180px] bg-white text-sm sm:text-lg"
+    style="transform: rotate({rotation}deg);"
+>
+    {#if side === "front"}
+        <div class="justify-center text-center">{front}</div>
+    {:else}
+        <div
+            class="p-1 h-full w-full border border-gray-400 bg-gray-300 rounded flex flex-col justify-center content-center"
+        >
+            <div class="justify-center text-center">{back}</div>
+        </div>
+    {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
     import Button from "$lib/components/Button.svelte";
-    import Card from "$lib/components/Card.svelte"
-    import { sharedState } from "$lib/state.svelte";
+    import Card from "$lib/components/Card.svelte";
     let side: "front" | "back" = $state("front");
 </script>
 
 <Card front="Hello" back="World" {side} />
-<Button onclick={() => side = side === "front" ? "back" : "front"}>
+<Button onclick={() => (side = side === "front" ? "back" : "front")}>
     Flip
-</Button>
-
-<Button onclick={() => {
-    if (sharedState.uiScale === "small") {
-        sharedState.uiScale = "large";
-    } else {
-        sharedState.uiScale = "small";
-    }
-}}>
-    Toggle Size (currently {sharedState.uiScale})
 </Button>
